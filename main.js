2e7f51f952ebb736e4663f6b1c59c83059237ec3
dilan-sachintha97 <dil97an@gmail.com>
2022-12-16 23:08:38 +0530

    set time

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ setInterval(updateClock,1000)
 
 function updateClock(){
     let myDate = new Date();
-    document.getElementById('clock').innerText=myDate;
+    let myHours = myDate.getHours()
+    let myMinutes = myDate.getMinutes()
+    let mySec = myDate.getSeconds()
+
+    let myTime = `${myHours}:${myMinutes}:${mySec}`
+    document.getElementById('clock').innerText=myTime;
 }
